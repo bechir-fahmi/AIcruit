9dff7455b30d6b6dd0e5bd85b2f6c68fa4d8f91f
bechir-fahmi <95341647+bechir-fahmi@users.noreply.github.com>
2025-02-20 16:27:30 +0100

    Add user controller with profile retrieval and user listing functionality

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/User');
+
+exports.getProfile = async (req, res) => {
+    res.json(req.user);
+};
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find().select('-password');
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
